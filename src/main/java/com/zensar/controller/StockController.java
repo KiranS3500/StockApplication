cbc8c01e8234ff2da5bc906065c1316517f14d18
KiranS3500 <kirans3500@gmail.com>
2022-02-08 19:48:45 +0530

    stocksApp latest

--- src/main/java/com/zensar/controller/StockController.java
+++ src/main/java/com/zensar/controller/StockController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -140,4 +141,5 @@ public ResponseEntity<List<Stock>> getStocksByNameLike(@PathVariable("nameLike")
 {
 	return new ResponseEntity<List<Stock>>(stockService.getStocksByNameLike(namelike),HttpStatus.OK);
 }
+
 }
